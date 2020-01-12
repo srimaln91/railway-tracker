fc61a9317c631d4d07cc70913d2e5258bf1ae630
srimal_n <srimal@pickme.lk>
2020-01-12 11:37:16 +0530

    Assigned a type for the promise

--- src/domain/usecases/location_check.ts
+++ src/domain/usecases/location_check.ts
@@ -9,7 +9,7 @@ export default class LocationChecker {
         this.railwayRepository = railwayRepository
     }
 
-    Check(location: Point, distance: number): Promise<any> {
+    Check(location: Point, distance: number): Promise<boolean> {
         return new Promise((resolve, reject) => {
             this.railwayRepository.CheckOnRailWay(location, distance).then((result) => {
                 resolve(result)
